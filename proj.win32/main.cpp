953edf43820b34fda17c827e516a89184cd694c2
buck84 <buck84@126.com>
2014-08-19 09:36:11 +0800

    add main

---
+++ proj.win32/main.cpp
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "AppDelegate.h"
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+	
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
+    // create the application instance
+    AppDelegate app;
+    int ret = Application::getInstance()->run();
+ 
+	FreeConsole();
+
+    return ret;
+}
